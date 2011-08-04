77462e958a3b350462d22c9b04834e7cc232a950
Steven Collins <steven.collins@it-agile.de>
2011-08-04 15:08:54 +0200

    removed client name, added uuid

--- ccc_kata_test.py
+++ ccc_kata_test.py
@@ -8,52 +8,76 @@
 """
 
 import sys
-from socket import *
+import uuid
+import socket
 from random import Random
 
+LOCALHOST = 'localhost'
+
 def add(a, b):
-    return a+b
+    return a + b
 
 def subtract(a, b):
-    return a-b
+    return a - b
 
 def multiply(a, b):
-    return a*b
+    return a * b
 
-functions = {'ADD': add, 'SUBTRACT': subtract, 'MULTIPLY': multiply}
+functions = {add: 'ADD', subtract: 'SUBTRACT', multiply: 'MULTIPLY'}
+
+def create_client(port):
+    addr = (LOCALHOST, port)
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return addr, client
+
+def create_server(port):
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server.bind((LOCALHOST, port))
+    return server
+
+def get_function(index):
+    return functions.keys()[index]
+
+def create_new_message(function, id, args):
+    message = [functions[function]]
+    message.append(id)
+    for arg in args:
+        message.append(str(arg))
+    message = u':'.join(message)
+    return message
+
+def create_new_args(number_of_args):
+    args = []
+    for j in xrange(0, number_of_args):
+        arg = random.randint(1, 200)
+        args.append(arg)
+    return args
+
+def create_expected_response(function, id, args):
+    result = reduce(function, args)
+    return u':'.join([id, str(result)])
 
 if __name__ == '__main__':
-    server_socket = socket(AF_INET, SOCK_DGRAM)
-    server_addr = ('127.0.0.1', int(sys.argv[1]))
-    own_socket = socket(AF_INET, SOCK_DGRAM)
-    own_socket.bind(('127.0.0.1', int(sys.argv[2])))
-    print 'waiting for client name...'
-    client_name = own_socket.recv(4096)
-    print 'client name: <%s>' % (client_name)
+    client_addr, client = create_client(int(sys.argv[1]))
+    server = create_server(int(sys.argv[2]))
     random = Random()
     random.seed()
     ok = True
     for i in xrange(0, 3):
-        key, function = functions.items()[random.randint(0,2)]
-        message = [key]
-        args = []
-        number_of_args = random.randint(2,5)
-        for j in xrange(0, number_of_args):
-            arg = random.randint(1, 200)
-            message.append(str(arg))
-            args.append(arg)
-        message = ':'.join(message)
-        result = reduce(function, args)
-        expected = ':'.join([client_name, str(result)])
-        print message
-        print 'expected: <%s>' % (expected)
-        server_socket.sendto(message, server_addr)
-        response = own_socket.recv(4096)
+        function = get_function(random.randint(0, 2))
+        args = create_new_args(random.randint(2, 5))
+        id = uuid.uuid4().hex
+        message = create_new_message(function, id, args)
+        print 'send: <%s>' % message
+        expected = create_expected_response(function, id, args)
+        print 'expected: <%s>' % expected
+        client.sendto(message, client_addr)
+        response = server.recv(4096)
         print 'received: <%s>' % (response)
         if response != expected:
             ok = False
 
-    server_socket.sendto(u'__SHUTDOWN__', server_addr)
+    client.sendto(u'__SHUTDOWN__', client_addr)
     if ok:
         print 'successful!'
     else:
